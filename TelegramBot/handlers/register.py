2214bd68b7cae1d10aa77c9cbbe00621ea2bb73c
user <antonsamulin@gmail.com>
2025-05-14 21:11:56 +0300

    Подстроил все хендлеры под view_arts и user_info

--- TelegramBot/handlers/register.py
+++ TelegramBot/handlers/register.py
@@ -5,7 +5,7 @@ from aiogram.fsm.state import StatesGroup, State
 
 from TelegramBot.keyboards.inline_kbs import send_register_kb, send_cancel_kb, send_retry_reg_kb, send_start_login_kb, \
     send_start_not_login_kb
-from config import SITE_API
+from config import SITE_API, BOT_TEXTS
 from .check_login import check_user_login_now
 
 import requests
@@ -25,39 +25,38 @@ async def register(call: CallbackQuery, state: FSMContext) -> None:
     if current_state is not None:  # если статус не установлен, то ничего не делаем
         await state.clear()
 
-    await call.message.edit_text('Выберите подходящий для вас способ регистрации.', reply_markup=send_register_kb())
+    await call.message.edit_text(BOT_TEXTS["choose_reg_way"], reply_markup=send_register_kb())
 
 
 @register_route.callback_query(F.data == 'telegram_reg')
 async def tg_reg(call: CallbackQuery, state: FSMContext):
     if call.from_user.username:
-        await call.message.edit_text('В качестве имени пользователя будет использовано ваше username.\n\n'
-                                     'Введите желаемый пароль', reply_markup=send_cancel_kb())
+        await call.message.edit_text(BOT_TEXTS["tg_info"], reply_markup=send_cancel_kb())
         await state.update_data(nick_name=call.from_user.username)
         await state.set_state(RegForm.password)
     else:
-        await call.message.edit_text("Для регистрации через Telegram необходимо указать username в настройках.",
+        await call.message.edit_text(BOT_TEXTS["tg_username_required"],
                                      reply_markup=send_retry_reg_kb())
     await call.answer()
 
 
 @register_route.callback_query(F.data == 'default_reg')
 async def set_nick_name(call: CallbackQuery, state: FSMContext):
-    await call.message.edit_text('Введите желаемое имя пользователя', reply_markup=send_cancel_kb())
+    await call.message.edit_text(BOT_TEXTS["ask_nickname"], reply_markup=send_cancel_kb())
     await state.set_state(RegForm.nick_name)
 
 
 @register_route.message(RegForm.nick_name)
 async def set_password(message: Message, state: FSMContext):
     await state.update_data(nick_name=message.text)
-    await message.answer('Введите пароль', reply_markup=send_cancel_kb())
+    await message.answer(BOT_TEXTS["ask_password"], reply_markup=send_cancel_kb())
     await state.set_state(RegForm.password)
 
 
 @register_route.message(RegForm.password)
 async def set_password_again(message: Message, state: FSMContext):
     await state.update_data(password=message.text)
-    await message.answer('Повторите введенный пароль', reply_markup=send_cancel_kb())
+    await message.answer(BOT_TEXTS["ask_repeat_password"], reply_markup=send_cancel_kb())
     await state.set_state(RegForm.password_again)
 
 
@@ -68,33 +67,22 @@ async def check_register(message: Message, state: FSMContext):
     data = await state.get_data()
     payload = {'nick_name': data['nick_name'],
                'password': data['password'],
-               'password_again': data['password_again']}
+               'password_again': data['password_again'],
+               'chat_id': message.chat.id}
 
-    try:
-        resp = requests.post(f'{SITE_API}/register', json=payload, timeout=5)
-        resp.raise_for_status()
-        resp_data = resp.json()
-    except ValueError:
-        return await message.answer('Некорректный ответ от сервера. Попробуйте позже.')
+    url = f'{SITE_API}/register'
 
-    if resp_data.get('success'):
-        try:
-            check_user_login_now(message.chat.id)
-            login_payload = {'nick_name': data['nick_name'],
-                             'password': data['password'],
-                             'chat_id': message.chat.id}
-            login_resp = requests.post(f'{SITE_API}/login', json=login_payload)
-            login_resp.raise_for_status()
-            login_resp_data = login_resp.json()
-        except requests.exceptions.HTTPError:
-            await message.answer(
-                'Не удалось автоматически войти в учетную запись, просим авторизоваться самостоятельно',
-                reply_markup=send_start_not_login_kb())
-        if login_resp_data.get('success'):
-            await message.answer(
-                'Регистрация прошла успешно, для заполнения остальных данных перейдите в раздел "Информация об учетной записи"',
-                reply_markup=send_start_login_kb())
+    reg_resp = requests.post(url, json=payload).json()
+
+    if reg_resp.get('success'):
+        url = f'{SITE_API}/login'
+
+        login_resp = requests.post(url, json=payload).json()
+
+        if login_resp.get('success'):
+            await message.answer(BOT_TEXTS["success_register"], reply_markup=send_start_login_kb())
+        else:
+            await message.answer(BOT_TEXTS["error_login_after_reg"],
+                                 reply_markup=send_start_not_login_kb())
     else:
-        await message.answer(
-            f"{resp_data.get('error', 'Неизвестная ошибка')}.",
-            reply_markup=send_retry_reg_kb())
+        await message.answer(reg_resp.get('user_message', BOT_TEXTS['other_error']), reply_markup=send_retry_reg_kb())
